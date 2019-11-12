091704a040af9698a97569dde084a5a9e9c65223
Daimee MacIsaac <gingah.ninja47@gmail.com>
2019-11-12 13:33:29 -0500

    Began Programming dialogue system and Character objects

---
+++ NoOneIsTheEnemy/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    [Tooltip("List of dialogue lines for the dialogue")]
+    public DialogueLine[] lines;
+
+    [Tooltip("Whether dialogue ends in a branch or the end of a conversation")]
+    public DialogueType type;
+
+    [Tooltip("Branch option text. Only used if Type is Branch. Should have 2-3 branches")]
+    public string[] branches;
+}
+
+[System.Serializable]
+public struct DialogueLine
+{
+    [Tooltip("The name to display with this line")]
+    public string name;
+
+    [Tooltip("The text of the dialogue line")]
+    [TextArea(3, 10)]
+    public string line;
+}
+
+public enum DialogueType
+{
+    branch,
+    end
+}
---
+++ NoOneIsTheEnemy/Assets/Scripts/ScriptableObjects/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character", menuName = "Character", order = 1)]
+public class Character : ScriptableObject
+{
+    [Tooltip("Character's full name, to be displayed at the top of text box when they talk")]
+    public string charName;
+
+    [Tooltip("Character's shortened name. Usually a first name.")]
+    public string shortName;
+
+    [Tooltip("Input pronouns in the order of this example: they, them, their, theirs, themself. You must include all five, even if they repeat.")]
+    public string[] pronouns = new string[5];
+
+    [Tooltip("List of possible dialogues")]
+    public Dialogue[] dialogues;
+
+    [Tooltip("Array of sprites. (Could only contain one)")]
+    public Sprite[] sprites;
+
+    private int relationship = -100;
+}
